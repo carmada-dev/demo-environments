e4670676e041ba80c467c5d604e5e197ced40757
Markus Heiliger <markus.heiliger@dash-two.de>
2023-08-23 10:52:32 +0200

    ...

--- SonarQube_PRIVATE/resources.tf
+++ SonarQube_PRIVATE/resources.tf
@@ -173,7 +173,7 @@ resource "azurerm_mssql_database" "SonarQube" {
 resource "azuread_application" "SonarQube" {
 	display_name 					= "${data.azurerm_resource_group.Environment.name}-${azurerm_linux_web_app.SonarQube.default_hostname}"
 	identifier_uris  				= [ "api://${data.azurerm_resource_group.Environment.name}-${azurerm_linux_web_app.SonarQube.default_hostname}" ]
-	owners 							= [ data.azuread_client_config.Current.client_id ]
+	# owners 							= [ data.azuread_client_config.Current.object_id ]
 	sign_in_audience 				= "AzureADMyOrg"
 
 	web {
